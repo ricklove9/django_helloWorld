0d06d0003b84c00173ea97a4556a87e0a3676b1d
Richard Loveland <ricky.loveland96@gmail.com>
2017-04-01 19:12:12 +0000

    finished the hello world and an updating time

---
+++ hello/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+import time
+# Create your views here.
+
+
+def index(request):
+	return HttpResponse("Hello World at " + time.strftime("%c"))
+
+
+
